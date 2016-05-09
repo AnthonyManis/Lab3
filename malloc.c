a48c29cd4f4c2ce900a6ac9cc2a2d9f4257a3fd2
rcockins <ryancockins@hotmail.com>
2016-05-09 13:50:41 -0700

    inital file with some notes about each function

---
+++ malloc.c
@@ -0,0 +1,41 @@
+// Anthony
+int allocate(int number_of_bytes)
+{
+	prints out a unique block number
+	start at 1 and increment
+	return block number
+}
+
+// Mukesh
+void free(int block_number)
+{
+	frees the memory
+}
+
+// Mukesh
+void blocklist()
+{
+	print out the heap
+	size	allocated	start	end (addresses in Hex)
+}
+
+// Ryan
+void writeheap(int the_block_number, char CTW, int copies)
+{
+	write characters to blocks in the heap
+}
+
+// Ryan
+void printheap(int block_number, int number_of_bytes)
+{
+	prints out all the bytes starting at the block number given
+}
+
+// Anthony
+int main()
+{
+	loop to call all functions
+	statically allocate initial array
+	interface
+}
+
