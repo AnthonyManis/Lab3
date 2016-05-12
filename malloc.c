c0f0a997ecf5a19d89909f45394efb456eb9ae01
AnthonyManis <anthonygmanis@gmail.com>
2016-05-11 18:20:17 -0700

    Spacing

--- malloc.c
+++ malloc.c
@@ -44,18 +44,18 @@ void initialize();
 
 int indexOfBlockNumber(int block_number)
 {
-	if (block_number == 1)
-		return 0;
+    if (block_number == 1)
+        return 0;
 
-	int i = 0;
+    int i = 0;
 
-	while (HeapArray[i].block_number != block_number)
-	{
-		i += HeapArray[i].size;
-	}
-	
-	return i;
-}	
+    while (HeapArray[i].block_number != block_number)
+    {
+        i += HeapArray[i].size;
+    }
+    
+    return i;
+}   
 
 // Anthony
 int allocate(int number_of_bytes)
@@ -74,10 +74,10 @@ int allocate(int number_of_bytes)
 
             // if we aren't allocating the whole block,
             // it must be split and a new header must be made
-            if (number_of_bytes < p->size) {
-                Node *next_header = p + number_of_bytes;
-                set_allocated(next_header, 0);
-                set_block_size(next_header, p->size - number_of_bytes);
+    if (number_of_bytes < p->size) {
+        Node *next_header = p + number_of_bytes;
+        set_allocated(next_header, 0);
+            set_block_size(next_header, p->size - number_of_bytes);
                 set_block_number(next_header, 0);
             }
             // finally, allocate the header
@@ -157,51 +157,51 @@ void set_block_size(Node *p, int size) {
 // (free is conflicting with C, needed to rename)
 void deallocate(int block_number)
 {
-	// frees the memory
+    // frees the memory
 }
 
 // Mukesh
 void blocklist()
 {
-	// print out the heap
-	// size	allocated	start	end (addresses in Hex)
+    // print out the heap
+    // size allocated   start   end (addresses in Hex)
 }
 
 // Ryan
 void writeheap(int block_number, char CTW, int copies)
 {
-	// write characters to blocks in the heap
-	// if the block number exists
-	// go to that block number
-	int index = indexOfBlockNumber(block_number);
-	int i;
-	int block_size = HeapArray[index].size;
-	// copy as many as the header of the block number says there are spaces
-	for (i = 0; index < HEAP_SIZE && i < block_size && i < copies; ++i)
-	{
-		// convert char to binary number
-		HeapArray[index].data = CTW;
-		++index;
-	}
+    // write characters to blocks in the heap
+    // if the block number exists
+    // go to that block number
+    int index = indexOfBlockNumber(block_number);
+    int i;
+    int block_size = HeapArray[index].size;
+    // copy as many as the header of the block number says there are spaces
+    for (i = 0; index < HEAP_SIZE && i < block_size && i < copies; ++i)
+    {
+        // convert char to binary number
+        HeapArray[index].data = CTW;
+        ++index;
+    }
 }
 
 // Ryan
 void printheap(int block_number, int number_of_bytes)
 {
-	// prints out all the bytes starting at the block number given
-	int index = indexOfBlockNumber(block_number);
-	int i;
-	for (i = 0; index < HEAP_SIZE && i < number_of_bytes; ++i)
-	{
-		printf("%c",HeapArray[index].data);
-		++index;
-	}
-	printf("\n");
+    // prints out all the bytes starting at the block number given
+    int index = indexOfBlockNumber(block_number);
+    int i;
+    for (i = 0; index < HEAP_SIZE && i < number_of_bytes; ++i)
+    {
+        printf("%c",HeapArray[index].data);
+        ++index;
+    }
+    printf("\n");
 }
 
 void quit() {
-	// free the heap we created woohoo!
-	free(HeapArray);
+    // free the heap we created woohoo!
+    free(HeapArray);
 }
 
 // shows a prompt to the user, gets an input line,
@@ -226,75 +226,75 @@ void promptUser() {
             // for (i = 0 ; i < argc ; i++) {
             //     printf("argv %d: %s\n", i, argv[i]);
             // }
-            if (!strcmp(argv[0], "allocate")) {
+                    if (!strcmp(argv[0], "allocate")) {
                 // first check that there's an appropriate number of arguments
-                if (argc == 2) {
-                	// int allocate(int number_of_bytes)
-                	int number_of_bytes = atoi(argv[1]);
-                	if (number_of_bytes > 0)
-                    	allocate(number_of_bytes);
-                }
-                else {
-                    printf("Usage: allocate [number_of_bytes]\n");
-                }
-            }
-            else if (!strcmp(argv[0], "free")) {
-                if (argc == 2) {
-                	// void deallocate(int block_number)
-                	int block_number = atoi(argv[1]);
-                	if (block_number > 0)
-                    	deallocate(block_number);
-                }
-                else {
-                    printf("Usage: free [block_number]\n");
-                }
-            }
-            else if (!strcmp(argv[0], "blocklist")) {
-            	// void blocklist()
-                blocklist();
-            }
-            else if (!strcmp(argv[0], "writeheap")) { 
-                if (argc == 4) {
-                	// void writeheap(int the_block_number, char CTW, int copies)
-                	int the_block_number = atoi(argv[1]);
-                	char CTW = argv[2][0];
-                	int copies = atoi(argv[3]);
-                	if ( (the_block_number > 0) && (copies > 0) )
-                    	writeheap(the_block_number, CTW, copies);
-                }
-                else {
-                    printf("Usage: writeheap [block_number] [char] [amount]\n");
-                }
-            }
-            else if (!strcmp(argv[0], "printheap")) {
-                if (argc == 3) {
-                	// void printheap(int block_number, int number_of_bytes)
-                	int block_number = atoi(argv[1]);
-                	int number_of_bytes = atoi(argv[2]);
-                	if ( (block_number > 0) && (number_of_bytes) > 0)
-                    	printheap(block_number, number_of_bytes);
-                }
-                else {
-                    printf("Usage: printheap [block_number] [amount]\n");
-                }
-            }
-            else if (!strcmp(argv[0], "quit")) {
-            	// void quit() 
-                quit();
-                break;
-            }
-            else {
-            	printf("Invalid Command\n");
-            }
-        }
-        if (argc != -1) {
-            int i;
-            for (i = 0 ; i < argc ; i++) {
-                free(argv[i]);
-            }
-        }
-        free(argv);
+                        if (argc == 2) {
+                    // int allocate(int number_of_bytes)
+                            int number_of_bytes = atoi(argv[1]);
+                            if (number_of_bytes > 0)
+allocate(number_of_bytes);
+}
+else {
+    printf("Usage: allocate [number_of_bytes]\n");
+}
+}
+else if (!strcmp(argv[0], "free")) {
+    if (argc == 2) {
+                    // void deallocate(int block_number)
+        int block_number = atoi(argv[1]);
+        if (block_number > 0)
+deallocate(block_number);
+}
+else {
+    printf("Usage: free [block_number]\n");
+}
+}
+else if (!strcmp(argv[0], "blocklist")) {
+                // void blocklist()
+    blocklist();
+}
+else if (!strcmp(argv[0], "writeheap")) { 
+    if (argc == 4) {
+                    // void writeheap(int the_block_number, char CTW, int copies)
+        int the_block_number = atoi(argv[1]);
+        char CTW = argv[2][0];
+        int copies = atoi(argv[3]);
+        if ( (the_block_number > 0) && (copies > 0) )
+    writeheap(the_block_number, CTW, copies);
+}
+else {
+    printf("Usage: writeheap [block_number] [char] [amount]\n");
+}
+}
+else if (!strcmp(argv[0], "printheap")) {
+    if (argc == 3) {
+                    // void printheap(int block_number, int number_of_bytes)
+        int block_number = atoi(argv[1]);
+        int number_of_bytes = atoi(argv[2]);
+        if ( (block_number > 0) && (number_of_bytes) > 0)
+printheap(block_number, number_of_bytes);
+}
+else {
+    printf("Usage: printheap [block_number] [amount]\n");
+}
+}
+else if (!strcmp(argv[0], "quit")) {
+                // void quit() 
+    quit();
+        break;
     }
+    else {
+        printf("Invalid Command\n");
+    }
+}
+if (argc != -1) {
+        int i;
+        for (i = 0 ; i < argc ; i++) {
+            free(argv[i]);
+        }
+    }
+    free(argv);
+}
 }
 
 // parses a line into command & any arguments,
@@ -308,31 +308,31 @@ int parseCommand(char *line, size_t *n, char ***tokens) {
         *n = 2;
         *tokens = malloc(*n * sizeof(char*));
         if (!*tokens)
-            return -1;
-    }
+return -1;
+}
 
- 
-    char *delims = " \t\n";
-    char *token_begin = NULL;
-    char *token_end = line;
-    size_t token_size = 0;
-    size_t count = 0;
 
-    while (*token_end) {
-        if (count >= *n) {
-            *n = *n * 2;
-            *tokens = realloc(*tokens, *n * sizeof(char*));
-            if (!*tokens)
-                return -1;
-        }
-        if (!strchr(delims, *token_end)) {
+char *delims = " \t\n";
+char *token_begin = NULL;
+char *token_end = line;
+size_t token_size = 0;
+size_t count = 0;
+
+while (*token_end) {
+    if (count >= *n) {
+    *n = *n * 2;
+    *tokens = realloc(*tokens, *n * sizeof(char*));
+    if (!*tokens)
+        return -1;
+}
+if (!strchr(delims, *token_end)) {
             // ignore leading whitespace
-            if (!token_begin) {
-                token_begin = token_end;
-            }
-            token_size++;
-            token_end++;
-        }
+    if (!token_begin) {
+        token_begin = token_end;
+    }
+    token_size++;
+    token_end++;
+}
         else { // new delimiter found, store the current token
             if (token_begin) {
                 (*tokens)[count] = malloc(token_size+1);
@@ -376,6 +376,6 @@ void initialize() {
 int main(int argc, char** argv)
 {
     initialize();
-	promptUser();
-	return 0;
+    promptUser();
+    return 0;
 }
